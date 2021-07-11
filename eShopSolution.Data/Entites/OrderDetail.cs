ddd7eb9255972d9136cc46619630bc069c31d324
Tinnd162 <tinnd162@gmail.com>
2021-07-11 14:58:14 +0700

    add Entities.

---
+++ eShopSolution.Data/Entites/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entites
+{
+    public class OrderDetail
+    {
+        public int OrderId { set; get; }
+        public Order Order { get; set; }
+        public int ProductId { set; get; }
+        public Product Product { get; set; }
+        public int Quantity { set; get; }
+        public decimal Price { set; get; }
+
+    }
+}
