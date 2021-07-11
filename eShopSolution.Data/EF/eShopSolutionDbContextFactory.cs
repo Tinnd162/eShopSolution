f6fbc8355ba52dece4809b4dadab27af538e32f7
Tinnd162 <tinnd162@gmail.com>
2021-07-11 15:02:13 +0700

    add DbContext.

---
+++ eShopSolution.Data/EF/eShopSolutionDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using eShopSolution.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class eShopSolutionDbContextFactory : IDesignTimeDbContextFactory<eShopDbContext>
+    {
+        public eShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json")
+                  .Build();
+
+            var connectionString = configuration.GetConnectionString("eShopSolutionDb");
+
+            var optionBuilder = new DbContextOptionsBuilder<eShopDbContext>();
+            optionBuilder.UseSqlServer(connectionString);
+
+            return new eShopDbContext(optionBuilder.Options);
+        }
+    }
+}
